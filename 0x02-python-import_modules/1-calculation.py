8977c8018ff83744ef1dcc138bdc2605fdbcf3e2
mohamedamine <boualiamino0123@gmail.com>
2024-01-19 06:58:50 -0800

    calcul

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import*
+    a = 10
+    b = 5
+    ad = add(a, b)
+    print("{} + {} = {}".format(a, b, ad))
+    su = sub(a, b)
+    print("{} - {} = {}".format(a, b, su))
+    mu = mul(a, b)
+    print("{} * {} = {}".format(a, b, mu))
+    di = div(a, b)
+    print("{} / {} = {}".format(a, b, di))
