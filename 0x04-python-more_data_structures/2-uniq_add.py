e6617c319fa5eb89607aade1df5877f082138838
mohamedamine <boualiamino0123@gmail.com>
2024-01-29 14:08:09 -0800

    sets

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,5 @@ def uniq_add(my_list=[]):
     unique_list = list(set(my_list))
     summ = 0
     for i in range(len(unique_list)):
-        summ+= unique_list[i]
+        summ += unique_list[i]
     return summ
