8fcb3f6b9ebe503c9198e81319b2efa150ad1920
Mohamed Amin Bouali <mohamedaminbuali6@ieee.org>
2024-03-21 06:37:21 -0700

    add safePrintDivision file

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
-    result = None
+    r = None
     try:
-        result = a / b
+        r = a / b
     except ZeroDivisionError:
         pass
     finally:
-        print("Inside result:{}".format(result))
-    return result
+        print("Inside result: {}".format(r))
+    return r
