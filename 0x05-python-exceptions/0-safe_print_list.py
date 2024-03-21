2c62371296e8c32a648271d039fa5cc07c85121b
Mohamed Amin Bouali <mohamedaminbuali6@ieee.org>
2024-03-21 05:39:25 -0700

    add safePrintList file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-            printedelm =+1
+            printedelm += 1
     except IndexError :
         pass
     print()
