73feff3855c24933853dd107853222de9ea02d14
Mohamed Amin Bouali <mohamedaminbuali6@ieee.org>
2024-03-22 07:15:21 -0700

    add second square file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py
+"""defines a square by Private instance attribute"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        self.__size = size
